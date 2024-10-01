0d9e41cb85895edbaf8316aefac80210b105e962
lucas066001 <chapuis863@gmail.com>
2024-10-01 13:07:24 +0200

    perf(historian): Adding a micro service in charge of absorbing acquisition flux and releasing loads on API

---
+++ SmartWeather.Historian/Configuration/PostStartup.cs
@@ -0,0 +1,35 @@
+﻿using SmartWeather.Services.ComponentDatas;
+using SmartWeather.Services.Components;
+using SmartWeather.Services.MeasurePoints;
+using SmartWeather.Services.Mqtt;
+using SmartWeather.Services.Mqtt.Handlers;
+using SmartWeather.Services.Stations;
+
+namespace SmartWeather.Historian.Configuration;
+
+public class PostStartup : IHostedService
+{
+    private readonly MqttSingleton _mqttService;
+    private readonly StationService _stationService;
+    private readonly ComponentService _componentService;
+    private readonly MeasureDataService _componentDataService;
+    private readonly MeasurePointService _measurePointService;
+
+    public PostStartup(MqttSingleton mqttService, IServiceScopeFactory scopeFactory)
+    {
+        _mqttService = mqttService;
+        _stationService = scopeFactory.CreateScope().ServiceProvider.GetRequiredService<StationService>();
+        _componentService = scopeFactory.CreateScope().ServiceProvider.GetRequiredService<ComponentService>();
+        _componentDataService = scopeFactory.CreateScope().ServiceProvider.GetRequiredService<MeasureDataService>();
+        _measurePointService = scopeFactory.CreateScope().ServiceProvider.GetRequiredService<MeasurePointService>();
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await _mqttService.ConnectAsync();
+        _mqttService.RegisterRequestHandler(new ConfigRequestHandler(_mqttService, _stationService, _componentService, _measurePointService));
+        _mqttService.RegisterMessageHandler(new MeasureDataMessageHandler(_mqttService, _componentDataService));
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+}
---
+++ SmartWeather.Historian/Program.cs
@@ -0,0 +1,32 @@
+using SmartWeather.Services;
+using SmartWeather.Repositories;
+using SmartWeather.Historian.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddRepositories(builder.Configuration);
+builder.Services.AddServices(builder.Configuration);
+builder.Services.AddHostedService<PostStartup>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
