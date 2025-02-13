63abbc433ccc7bc443c47e4b14b719b3b6cf5c28
lucas066001 <chapuis863@gmail.com>
2025-02-13 11:39:12 +0100

    chore(presentation.WebClient): Preparing components for station configuration

---
+++ Presentation/SmartWeather.WebClient/src/app/components/organisms/configurator-station/configurator-station.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-configurator-station',
+  imports: [],
+  templateUrl: './configurator-station.component.html',
+  styleUrl: './configurator-station.component.css'
+})
+export class ConfiguratorStationComponent {
+
+}
--- Presentation/SmartWeather.WebClient/src/app/pages/configurator/configurator.component.ts
+++ Presentation/SmartWeather.WebClient/src/app/pages/configurator/configurator.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ConfiguratorSelectorComponent } from '@components/organisms/configurator-selector/configurator-selector.component';
+import { ConfiguratorStationComponent } from '@components/organisms/configurator-station/configurator-station.component';
 import { DashboardTemplateComponent } from '@templates/dashboard-template/dashboard-template.component';
 
 @Component({
   selector: 'app-configurator',
-  imports: [DashboardTemplateComponent, ConfiguratorSelectorComponent],
+  imports: [DashboardTemplateComponent, ConfiguratorSelectorComponent, ConfiguratorStationComponent],
   templateUrl: './configurator.component.html',
   styleUrl: './configurator.component.css'
 })
