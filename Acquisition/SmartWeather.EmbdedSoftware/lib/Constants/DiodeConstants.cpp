2642a99c156fb70262d48f0734c7b8ea7afbb341
lucas066001 <chapuis863@gmail.com>
2025-02-27 17:12:19 +0100

    chore(acquisition.embdedSoftware): Updating pins to current station state

--- Acquisition/SmartWeather.EmbdedSoftware/lib/Constants/DiodeConstants.cpp
+++ Acquisition/SmartWeather.EmbdedSoftware/lib/Constants/DiodeConstants.cpp
@@ -1,7 +1,12 @@
 #include <DiodeConstants.h>
 
-namespace SmartWeather::Constants {
-    int BLUE_DIODE_PIN = 27;
-    int GREEN_DIODE_PIN = 14;
-    int RED_DIODE_PIN = 13;
+namespace SmartWeather::Constants
+{
+    int BLUE_DIODE_PIN = 16;
+    int GREEN_DIODE_PIN = 22;
+    int RED_DIODE_PIN = 19;
 }
+
+// Rouge -> 19
+// Bleu -> 16
+// Blanc -> 22
